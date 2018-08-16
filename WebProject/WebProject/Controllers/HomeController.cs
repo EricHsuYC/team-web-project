92fd9db5969ed1ea3bceba34284b938e538d49d8
EricYC <aerchit@gmail.com>
2018-08-16 16:50:10 +0800

    add App_Class 作為session管理 add HomeController

---
+++ WebProject/WebProject/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebProject.Models;
+using WebProject.App_Class;
+
+namespace WebProject.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        team_web_projectEntities db = new team_web_projectEntities();
+        
+
+        // GET: Home
+        public ActionResult Index()
+        {
+            if (appClass.Member != "")
+            {
+
+            }
+
+
+            return View();
+        }
+
+        
+    }
+}
